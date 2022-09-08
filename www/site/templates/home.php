bc2c6715aec62ac9b1b96b700f706de9dc9617bb
Timothy Achumba <timothyachumba@gmail.com>
2022-09-08 23:43:23 +0100

    added subtext and logo to the homepage

--- www/site/templates/home.php
+++ www/site/templates/home.php
@@ -2,10 +2,10 @@
 
 <?php 
 
-  $minWidth = 40;
+  $minWidth = 50;
   $maxWidth = 100;
-  $minHeight = 40;
-  $maxHeight = 130;
+  $minHeight = 80;
+  $maxHeight = 150;
   $minOffset = 0;
   $maxOffset = $maxWidth - $minWidth;
   $align = array('left','right');
@@ -21,7 +21,6 @@
     return random_color_part() . random_color_part() . random_color_part();
   }
 
-
 ?>
 
 
@@ -29,28 +28,37 @@
   <div class="grid" id="gallery">
     <?php foreach($projects as $key => $projectsColumn): ?>
       <div class="col col-1">
+        <?php
+            if ($key === array_key_first($projects)) {
+              snippet('logo');
+            }
+        ?>
         <?php foreach($projectsColumn as $project): ?>
           <?php 
             $width = rand($minWidth,$maxWidth);
             $height = rand($minHeight,$maxHeight);
             $maxOffset = $maxWidth - $width;
             $offset = rand($minOffset,$maxOffset);
+            $padding = rand(80,400);
             $key = array_rand($align);
             $flexAlign = 'start';
             if ($key = 1) {
               $flexAlign = 'end';
             };
           ?>
-          <div
-            class="gallery-image-wrapper"
-            style="display:flex;flex-direction:column;align-items:flex-<?= $flexAlign ?>">
-            <div
-              style="
-                width:<?= $width ?>%;
-                padding-bottom:<?= $height ?>%;
-                margin-<?= $align[$key] ?>:<?= $offset ?>%;
-                background-color:#<?= random_color() ?>">
-            </div>
+          
+          <div class="project-wrapper" style="display:flex;flex-direction:column;align-items:flex-<?= $flexAlign ?>;padding-top:<?= $padding ?>px">
+              <div
+                class="project"style="width:<?= $width ?>%;margin-<?= $align[$key] ?>:<?= $offset ?>%">
+                  <div class="project-image"style="padding-bottom:<?= $height ?>%;;background-color:#<?= random_color() ?>">
+                  </div>
+                  <div class="project-text">
+                    <h3 class="project-name">Project name</h3>
+                    <p class="project-meta"><span>Project Cateogry</span>, <span>Client</span></p>
+                  </div>
+                
+              </div>
+              
           </div>
         <?php endforeach; ?>
       </div>
