0ba8248f8dc78aad48fb89a82f17e5cfa4db97e2
Timothy Achumba <timothyachumba@gmail.com>
2022-09-12 22:20:31 +0100

    fixes for panel

--- www/site/templates/home.php
+++ www/site/templates/home.php
@@ -38,8 +38,8 @@
           <div class="project-wrapper" style="display:flex;flex-direction:column;align-items:flex-<?= $flexAlign ?>;padding-top:<?= $padding ?>px">
               <div
                 class="project"style="width:<?= $width ?>%;margin-<?= $align[$key] ?>:<?= $offset ?>%">
-                  <?php if($image = $project->cover()): ?>
-                    <img class="project-image" src="<?= $image->toFile()->url() ?>" alt="">
+                  <?php if($image = $project->cover()->toFile()): ?>
+                    <img class="project-image" srcset="<?= $image->srcset([800, 1200, 1600]) ?>" alt="">
                   <?php endif ?>
                   <div class="project-text">
                     <h3 class="project-name"><?= $project->title() ?></h3>
