1a9dd8a864edb77f3c7436f73f0ce0447476c753
Timothy Achumba <timothyachumba@gmail.com>
2022-10-22 17:12:32 +0100

    fix for gallery

--- www/site/snippets/footer.php
+++ www/site/snippets/footer.php
@@ -1,8 +1,8 @@
   <footer data-scroll-section>
     <div>&copy; <?= date('Y') ?> • Judith Achumba-Wöllenstein</div>
     <ul>
-      <li><a target="_blank"href="https://hajinsky.com">HAJINSKY</a></li>
       <li><a target="_blank"href="https://atelier.akuko.studio">Atelier Akuko</a></li>
+      <li><a target="_blank"href="https://hajinsky.com">HAJINSKY</a></li>
       <li><a target="_blank"href="https://instagram.com/atelierakuko">Instagram</a></li>
       <li><a target="_blank"href="mailto:judith@akuko.studio?subject=Hi!">Email</a></li>
     </ul>
