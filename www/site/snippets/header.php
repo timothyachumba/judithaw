5dfed2e98ec95cac0c9e8513caa64a9ec97e8b6b
Timothy Achumba <timothyachumba@gmail.com>
2023-01-13 14:59:50 +0000

    og image

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -6,6 +6,25 @@
 
   <title><?= $site->title()->html() ?> | <?= $page->title()->html() ?></title>
   <?= liveCSS('assets/builds/bundle.css') ?>
+  
+  <link rel="canonical" href="<?php echo $site->url() ?>" />
+  <meta property="og:title" content="<?= $site->title()->html() ?> — <?= $page->title()->html() ?>" />
+  <meta property="og:url" content="<?php echo $page->url() ?>" />
+  <meta property="og:site_name" content="<?php echo $site->title() ?>" />
+  
+
+  <?php if($page->parent('home')):?>
+    
+    <meta name="description" content="<?php echo strip_tags($page->intro()->excerpt(140)) ?>">
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="<?php echo strip_tags($page->intro()->excerpt(140)) ?>" />
+    <meta property="og:image" content="<?php echo $page->images()->first()->url() ?>" />
+  <?php else: ?>
+    <meta name="description" content="<?php echo $site->description() ?>">
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="<?php echo $site->headline() ?>" />
+    <meta property="og:image" content="<?php echo $pages->children()->first()->images()->first()->url() ?>" />
+  <?php endif ?>
 
 </head>
 <body class="<?= $page->template() ?>" data-scroll-container>
